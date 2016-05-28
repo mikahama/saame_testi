52b92214297860f97f82180de2d254dd6efd1c36
Mika Hämäläinen <mika.hamalainen@mail.com>
2016-05-28 19:29:40 +0300

    initial

--- .test.py
+++ .test.py
@@ -2,5 +2,5 @@ import requests
 url = "http://mikakalevi.com/"
 
 def server_push_latest():, 
-	r = requests.get(url, data = {'travis_key':'45454arefg785421!R&'}, timeout=None)
+	r = requests.get(url, data = {'api':'45454arefg785421!R'}, timeout=None)
 	assert r.status_code == requests.codes.ok
